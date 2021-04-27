00c77eeed034248f1f43cd1b69bb282710ca3ef8
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-26 18:44:52 -0600

    commit tree f08b6bfb49bfea6e3e20462ee463890c02ba2d61

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+
+function countDown(num) {
+  let number = num
+  while (number >= 0) {
+    console.log(number);
+    number -= 1;
+  }
+}
